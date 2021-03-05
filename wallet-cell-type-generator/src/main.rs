d856cf367015138560e7424876265bd02a6bf6fc
Link <xieaolin@gmail.com>
2021-03-05 10:50:45 +0800

    fix: wallet data output format

--- wallet-cell-type-generator/src/main.rs
+++ wallet-cell-type-generator/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let action_witness = das_util::wrap_action_witness("create_wallet", None);
 
     println!(
-        "0xb7526803f67ebe70aba6 0x{}  ",
+        "0xb7526803f67ebe70aba6  0x{} ",
         hex_string(action_witness.as_reader().raw_data()).unwrap()
     );
 }
